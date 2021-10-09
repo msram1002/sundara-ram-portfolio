f71a127f32c802a4fc12f5d4ce7073a0f4823971
Sundara Ram Matta <msram0210@gmail.com>
2021-10-08 23:47:49 -0400

    adding the hero page containing the intro title and intro text

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,10 +4,20 @@ import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponent
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
-const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+const Hero = () => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Hello, I'm Sundara Ram
+      </SectionTitle>
+      <SectionText>
+      I am a front-end web developer building out immersive and beautiful web applications and I find happiness in being a better me today than the me yesterday.
+      </SectionText>
+      <Button>
+        Email
+      </Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
